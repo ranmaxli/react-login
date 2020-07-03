92c4e6ce9ee7486824c1d8108e409cb903ece40b
RanLi <1206038126@qq.com>
2020-07-03 10:01:05 +0800

    add login code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+import 'antd/dist/antd.css'
+import axios from 'axios'
 
 function App() {
+
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+
+  const onFinish = values => {
+    axios.get('/user/login', {
+      params: {
+        username: values.username,
+        password: values.password
+      }
+    })
+    .then(function (response) {
+      if (response.data.code === 1) {
+        alert(response.data.data.dispaly_name);
+      } else {
+          alert(response.data.msg);
+      }
+    })
+    .catch(function (error) {
+      console.log(error.message);
+    });
+  };
+  
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Form
+    {...layout}
+    name="basic"
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+  >
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: 'Please input your username!' }]}
+    >
+      <Input style={{ width:300}}/>
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[{ required: true, message: 'Please input your password!' }]}
+    >
+      <Input.Password style={{ width:300}}/>
+    </Form.Item>
+
+    <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item {...tailLayout}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+  </div>
   );
 }
 
